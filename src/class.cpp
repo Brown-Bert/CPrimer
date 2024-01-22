562c438541ab42215b19fd48f66c3bd75cb0c51a
yzx <yzx.com>
2024-01-22 13:42:18 +0800

    2024-1-22

--- src/class.cpp
+++ src/class.cpp
@@ -10,23 +10,34 @@ public:
 };
 struct base
 {
-    int mem(){
-        puts("hello");
-        return 0;
-    }
+public:
+    base() = default;
+    base(int val) : a(val){};
+    base(base &&b) : a(b.a){
+        std::cout << "hello" << std::endl;
+    };
+    base(base &b) = default;
+    // virtual void func();
+public:
+    int a = 0;
 };
 
 struct D : public base
 {
-    int mem(int){
-        puts("jg");
-        return 0;
-    }
+    D() : base(){};
+    D(int val1, int val2) : base(val1), b(val2){};
+    D(D&& d) : base(std::move(d)) , b(d.b){
+        // 如果是指针的话提交了权限之后，需要把移交源的指针清空
+    };
+    D(D &d) : base(d){};
+public:
+    int b = 1;
 };
 int main(int argc, char* argv[])
 {
-    base b;
-    D d;
-    b.mem();
+    D d(4, 8);
+    D p(std::move(d));
+    std::cout << p.a << std::endl;
+    std::cout << p.b << std::endl;
     exit(0);
 }
